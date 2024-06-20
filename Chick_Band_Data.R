c9aae5e34ed662a9b59fb7ae81d96232d033aa96
CaitWalk <Caitlin.Walker@uga.edu>
2024-06-20 14:13:36 -0400

    notes added

--- Chick_Band_Data.R
+++ Chick_Band_Data.R
@@ -7,6 +7,7 @@ library(tidyverse)
 #Excel files:
 # Brood_Info_2000_2017
 # ChickcaptureData_No_dates
+# BroodCaptures2018-2023
 
 ChickCaptureData <- ChickCaptureData_No_Dates
 
@@ -29,8 +30,53 @@ ChickCaptureData <- ChickCaptureData %>% relocate(Date,.after = CaptureID) #move
 #remove chin marks data
 ChickCaptureData <- ChickCaptureData %>%
   filter(` Bird ID type` != "Chin Marks")
+#remove chin marks data
+ChickCaptureData <- ChickCaptureData %>%
+  filter(` Bird ID type` != "GB")
+
+#removed dead and released unmarked chicks from incubator
+ChickCaptureData <- ChickCaptureData %>%
+  filter(` Bird ID type` != "Other")
+ChickCaptureData <- ChickCaptureData %>%
+  filter(` BirdCondition` != "Dead")
+
+#remove the 2018 chicks
+ChickCaptureData <- ChickCaptureData %>%
+  filter(` Brood ID` != "640")
+
+#replace 14.25 with wing band for chick 102022
+ChickCaptureData$` Bird ID type`[ChickCaptureData$` Bird ID type`== '14.25'] <- 'Wing Band'
+
+#rename column
+colnames(ChickCaptureData)[4] <- "Chick ID" 
+colnames(ChickCaptureData)[2] <- "Capture Date"
+colnames(ChickCaptureData)[9] <- "Condition"
+
+# remove the T in front of the chickID --> extract starting as second character 
+BroodCaptures2018_2023$`Chick ID` <- substring(BroodCaptures2018_2023$`Chick ID`, 2)
+
+BroodCaptures2018_2023$`Chick ID` <- as.numeric(BroodCaptures2018_2023$`Chick ID`) #change str
+
+BroodCaptures2018_2023 <- BroodCaptures2018_2023%>%
+  filter(`Cond` != "Dead")
+
+colnames(BroodCaptures2018_2023)[12] <- "Condition"
 
 
- 
+#merge the 2 df into a single df that contains all chicks captured
+tagged_chicks <- merge(ChickCaptureData, BroodCaptures2018_2023, 
+                       by = c("Capture Date", "Chick ID"), all = TRUE)
+
+colnames(tagged_chicks)[8] <- "Weight.x"
+colnames(tagged_chicks)[18] <- "Weight.y"
+
+tagged_chicks <- tagged_chicks %>%
+  mutate(Weight = coalesce(Weight.x, Weight.y), #combine weight columns
+         Condition = ifelse(is.na(Condition.x), Condition.y, Condition.x)) #combines Condition 
+
+tagged_chicks <- select(tagged_chicks,-starts_with("Weight."), -starts_with("Condition.")) #remove the excess columns created in the merge
+
+
+
 
 
